e9a13030c5e11d07975eb8df4acd4fb0ac80e1aa
p455w0rd <p455w0rd@gmail.com>
2019-06-08 17:59:33 -0400

    update build stuff; bump version

--- src/main/java/p455w0rd/sct/init/ModGlobals.java
+++ src/main/java/p455w0rd/sct/init/ModGlobals.java
@@ -4,7 +4,7 @@ public class ModGlobals {
 
 	public static final String MODID = "sct";
 	public static final String MOD_NAME = "StoneCraftingTable";
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 	public static final String SERVER_PROXY = "p455w0rd.sct.proxy.CommonProxy";
 	public static final String CLIENT_PROXY = "p455w0rd.sct.proxy.ClientProxy";
 	public static final String DEPENDENCIES = "";
